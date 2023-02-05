abebf0912d700458b6b3307b15ae702036fab805
Sebastianidm <sebastiandiazmiranda@gmail.com>
2023-02-05 18:43:27 -0300

    Se sube el proyecto

---
+++ appclimaseba/src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+import Main from "./components/Main"
+ 
+function App() {
+
+  return (
+    <>
+        <div className="  bg-slate-900 h-screen  ">
+        <Main/>
+        </div>
+    </>
+  )
+}
+
+export default App
